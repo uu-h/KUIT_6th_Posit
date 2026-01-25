ca1ef264df0f90a00b97087e8920d1d6a2517d26
heexion <alsgml030901@naver.com>
2026-01-25 12:15:42 +0900

    feat: 메인화면 상단 검색바 UI 추가

--- src/pages/Guest/Main/Home.tsx
+++ src/pages/Guest/Main/Home.tsx
@@ -1,12 +1,18 @@
 import BottomSheet from "../../../components/Guest/Main/BottomSheet";
 import PopularPlaces from "../../../components/Guest/Main/PopularPlaces";
 import BottomBar from "../../../components/BottomBar/BottomBar";
+import SearchBar from "../../../components/Common/SearchBar";
 
 export default function Home() {
   return (
     <div className="relative w-full h-screen overflow-hidden">
       {/* 지도 영역 */}
       <div className="absolute inset-0 bg-neutrals-02" />
+
+      {/* 상단 검색 바 */}
+      <div className="absolute top-4 left-4 right-4 z-20">
+        <SearchBar />
+      </div>
 
       {/* Bottom Sheet */}
       <BottomSheet>
