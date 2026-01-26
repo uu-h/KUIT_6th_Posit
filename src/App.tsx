048926b9c61153b59be0f83663b112af0d7960eb
chaeeun <0_chaeeun@naver.com>
2026-01-26 15:35:55 +0900

    App.tsx 복원

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
-// import Router from "./routes/Router";
-import Home from "./pages/Guest/Main/Home";
+import Router from "./routes/Router";
 
 function App() {
   return (
     <div className="min-h-screen flex justify-center">
       <div className="w-[375px] min-h-screen flex justify-center">
         <BrowserRouter>
-          {/* <Router /> */}
-          <Home></Home>
+          <Router />
         </BrowserRouter>
       </div>
     </div>
