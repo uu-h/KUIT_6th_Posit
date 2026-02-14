480656554fd8a1ea848ac8d4d2173200b87a4389
chaeeun <0_chaeeun@naver.com>
2026-02-14 17:31:43 +0900

    fix: posit 작성 후 라우팅 연결 문제 해결

--- src/pages/Guest/Posit/GuestPositCreatePage.tsx
+++ src/pages/Guest/Posit/GuestPositCreatePage.tsx
@@ -178,7 +178,14 @@ export default function GuestPositCreatePage() {
         onConfirm={() => {
           setSuccessOpen(false);
           // TODO: 확인 누르면 어디로 갈지 (일단은 가게 상세로 구현)
-          navigate(`/stores/${sid}`, { state: { refreshPosit: true } });
+          navigate(`/stores/${sid}`, {
+            replace: true,
+            state: {
+              refreshPosit: true,
+              from: "home",
+              restore: location.state?.restore,
+            },
+          });
         }}
       />
 
